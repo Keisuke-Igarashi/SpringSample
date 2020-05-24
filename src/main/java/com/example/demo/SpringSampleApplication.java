951c4efd2f1fabb6322d45559eed962fa307ffe9
Keisuke_igarashi's_macbookpro <igarashi4948@gmail.com>
2020-05-24 20:19:22 +0900

    mavenビルドでwarファイルを作成するように修正

--- src/main/java/com/example/demo/SpringSampleApplication.java
+++ src/main/java/com/example/demo/SpringSampleApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSampleApplication {
+public class SpringSampleApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringSampleApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSampleApplication.class, args);
 	}
